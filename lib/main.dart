2b70f53996dcf4452ea355d870e7ac2cf1b93d83
rurikokawano <ruriko.k.0610@gmail.com>
2023-11-28 17:06:47 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,108 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Column buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildButtonColumn(color, Icons.call, 'CALL'),
+        buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Flutter layout demo"),
         ),
-        body: Center(
-          child: Container(
-            height: 100,
-            width: 200,
-            padding: const EdgeInsets.all(20),
-            color: Colors.blueGrey,
-            child: const Column(
-              children: [Text("Hello"), Icon(Icons.star)],
+        body: ListView(
+          children: [
+            Image.asset(
+              "images/lake.jpg",
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-          ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
     );
   }
 }
-// Widget titelSection = Container(
-//   padding const EdgeInsets.all(32),
-//   child:const Row(
-//     children: [
-//       Expanded(
 
-//       )
-//       ,
-//     ],
-//   ),
-// );
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: const Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
+//title section
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text(
+                "Oeschiren Lake Campground",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              "Kandersteg,Switzerland",
+              style: TextStyle(
+                color: Colors.grey[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+      Icon(Icons.star, color: Colors.red[500]),
+      const Text("41"),
+    ],
+  ),
+);
